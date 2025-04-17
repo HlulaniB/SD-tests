11e11e4bfebf9f49b92f29c1445d50bb947760d8
HlulaniB <baloyihlulani91@gmail.com>
2025-04-17 22:37:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import Addproducts from './add_products.js'
+
+ReactDom.createRoot(document.getElementById('#root')).render(<Addproducts/>);
